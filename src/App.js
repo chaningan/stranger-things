e02804ea9590e37fe2fb97ab29400b3d83700515
chaningan <chaningan@gmail.com>
2022-07-01 10:16:10 -0700

    strangers

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+import {
+  Home,
+  Logout,
+  Messages,
+  NavBar,
+  Posts,
+  Profile,
+  Users,
+} from "components";
+
 export default function App() {
-  return <></>
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </div>
+  );
 }
